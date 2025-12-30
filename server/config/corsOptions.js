a5edd781cc01c2054775a6d98f58adff29016bed
Roger Calaf <rcalaf@Rogers-MacBook-Pro.local>
2025-12-30 20:51:51 +0100

    setting up cors

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -3,24 +3,18 @@ const allowedOrigins = require('./allowedOrigins');
 const corsOptions = {
 
     origin: (origin, callback) => {
-       console.log(origin)
+      // console.log(origin)
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    // allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
     preflightContinue: false,
     credentials: true,
     // maxAge:600,
     optionsSuccessStatus: 200
 }
-
-// const corsOptions = {
-//     origin: true,
-//     optionsSuccessStatus: 200
-// }
-
 
 module.exports = corsOptions;
